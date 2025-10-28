291775d9e84c3adf796f7f4a1e53c79f5517468d
yajneshx94 <yajneshrajan83@gmail.com>
2025-10-28 14:29:09 +0530

    code url update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://linknest-api-d2ym.onrender.com'
+    baseURL: 'https://linknest-api-x9to.onrender.com'
 });
 
 api.interceptors.request.use(config => {
